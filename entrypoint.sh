55a743efd9cb438492e2aae4e356d285f0be8345
Maxim Matveev <maxim.m@oneupgames.gg>
2023-09-27 11:54:20 +0300

    no message

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -e
 
-appcenter distribute release --token "71a3b8b3d39553393d9b3d003af8feaba2fff0ae" --app "OneUp-Games/GG" --group "Collaborators" --file "build/Android/Android-4886-6322819200.apk" --release-notes "RELEASE_NOTES"
+appcenter distribute release --token "71a3b8b3d39553393d9b3d003af8feaba2fff0ae" --app "OneUp-Games/GG" --group "Collaborators" --file "build/Android/Android-4886-6322819200.apk"
 
 echo "releaseId=17" >> $GITHUB_OUTPUT
 
